3dc6850db22e9ac32b0fbe90adb97aee2716af3a
Vladimir Bogretsov <bogrecov@gmail.com>
2023-09-23 18:25:47 +0300

    v0.9.2

--- lua/core/theme.lua
+++ lua/core/theme.lua
@@ -6,7 +6,7 @@ vim.g.everforest_background = "soft"
 vim.g.everforest_show_eob = 0
 vim.g.everforest_better_performance = 1
 
-if vim.fn.exists("g:neovide") then
+if vim.fn.has("gui_running") ~= 0 then
   vim.g.everforest_transparent_background = 0
 else
   vim.g.everforest_transparent_background = 1
