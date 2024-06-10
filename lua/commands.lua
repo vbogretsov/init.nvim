bb0f302d8128f7f6b642c2f888daff1508d84f8a
Vladimir Bogretsov <bogrecov@gmail.com>
2024-06-11 01:09:02 +0300

    Upadte

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,2 +1,4 @@
 vim.cmd([[command! Fmt lua vim.lsp.buf.format({async = true})]])
-vim.cmd([[command! -nargs=1 -complete=help H :enew | :set buftype=help | :h <args>]])
+vim.cmd(
+  [[command! -nargs=1 -complete=help H :enew | :set buftype=help | :h <args>]]
+)
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -332,3 +332,87 @@ vim.api.nvim_set_keymap(
   "<cmd>Telescope git_status<CR>",
   { noremap = true }
 )
+
+-- Search
+vim.api.nvim_set_keymap(
+  "n",
+  "n",
+  [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "N",
+  [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "*",
+  [[*<Cmd>lua require('hlslens').start()<CR>]],
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "#",
+  [[#<Cmd>lua require('hlslens').start()<CR>]],
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "g*",
+  [[g*<Cmd>lua require('hlslens').start()<CR>]],
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "g#",
+  [[g#<Cmd>lua require('hlslens').start()<CR>]],
+  { noremap = true, silent = true }
+)
+
+-- venn.nvim: enable or disable keymappings
+function _G.Toggle_venn()
+  local venn_enabled = vim.inspect(vim.b.venn_enabled)
+  if venn_enabled == "nil" then
+    vim.b.venn_enabled = true
+    vim.cmd([[setlocal ve=all]])
+    -- draw a line on HJKL keystokes
+    vim.api.nvim_buf_set_keymap(
+      0,
+      "n",
+      "J",
+      "<C-v>j:VBox<CR>",
+      { noremap = true }
+    )
+    vim.api.nvim_buf_set_keymap(
+      0,
+      "n",
+      "K",
+      "<C-v>k:VBox<CR>",
+      { noremap = true }
+    )
+    vim.api.nvim_buf_set_keymap(
+      0,
+      "n",
+      "L",
+      "<C-v>l:VBox<CR>",
+      { noremap = true }
+    )
+    vim.api.nvim_buf_set_keymap(
+      0,
+      "n",
+      "H",
+      "<C-v>h:VBox<CR>",
+      { noremap = true }
+    )
+    -- draw a box by pressing "f" with visual selection
+    vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", { noremap = true })
+  else
+    vim.cmd([[setlocal ve=]])
+    vim.cmd([[mapclear <buffer>]])
+    vim.b.venn_enabled = nil
+  end
+end
+-- toggle keymappings for venn using <leader>v
+-- vim.api.nvim_set_keymap('n', '<leader>v', ":lua Toggle_venn()<CR>", { noremap = true})
