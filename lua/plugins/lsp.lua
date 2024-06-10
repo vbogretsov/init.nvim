5587a5b200b500e65ac13055f21aa605dc27c78b
Vladimir Bogretsov <bogrecov@gmail.com>
2024-06-11 01:07:47 +0300

    Upadte

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -101,21 +101,21 @@ local function setup()
     on_init = function(client)
       local path = client.workspace_folders[1].name
       if
-          vim.loop.fs_stat(path .. "/.luarc.json")
-          or vim.loop.fs_stat(path .. "/.luarc.jsonc")
+        vim.loop.fs_stat(path .. "/.luarc.json")
+        or vim.loop.fs_stat(path .. "/.luarc.jsonc")
       then
         return
       end
       client.config.settings.Lua =
-          vim.tbl_deep_extend("force", client.config.settings.Lua, {
-            runtime = {
-              version = "LuaJIT",
-            },
-            workspace = {
-              checkThirdParty = false,
-              library = vim.api.nvim_get_runtime_file("", true),
-            },
-          })
+        vim.tbl_deep_extend("force", client.config.settings.Lua, {
+          runtime = {
+            version = "LuaJIT",
+          },
+          workspace = {
+            checkThirdParty = false,
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+        })
     end,
     settings = {
       Lua = {},
