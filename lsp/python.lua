da06171327ba51bc11e1fb4fd42eef8ffc86028a
Vladimir Bogretsov <vladimir.bogretsov@inspectorio.com>
2025-06-10 17:43:37 +0300

    Switched back to pyright

--- lsp/python.lua
+++ lsp/python.lua
@@ -1,4 +1,4 @@
-return {
+--[[ return {
   cmd = { "basedpyright-langserver", "--stdio" },
   filetypes = { "python" },
   root_markers = {
@@ -7,6 +7,33 @@ return {
   },
   settings = {
     basedpyright = {
+      analysis = {
+        autoImportCompletions = true,
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+      },
+    },
+  },
+  capabilities = {
+    textDocument = {
+      completion = {
+        completionItem = {
+          snippetSupport = true,
+        }
+      }
+    }
+  }
+} ]]
+
+return {
+  cmd = { "pyright-langserver", "--stdio" },
+  filetypes = { "python" },
+  root_markers = {
+    "pyproject.toml",
+    ".git",
+  },
+  settings = {
+    pyright = {
       analysis = {
         autoImportCompletions = true,
         autoSearchPaths = true,
